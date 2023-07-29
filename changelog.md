1d615c2eeb4d34fae27e4a93535ea84ba34082f9
ajchen02 <106304864@qq.com>
2023-07-29 23:53:54 +0800

    clone of TMTable

---
+++ changelog.md
@@ -0,0 +1,16 @@
+# The Modding Table changelog:
+
+# v3.0.1.7 = 25/7/23
+	 - Fix a bug again.
+	 - Modify the text again.
+	 - Added a doc( only same as The Modding Tree ).
+
+# v3.0.0.6 = 25/7/23
+	 - Fix a bug.
+	 - Modify the text.
+
+# v3.0.0.4 = 25/7/23
+	 - Fix tow bugs.
+
+# v3.0 = 23/7/23
+	 - Developed The Modding Table, Which, you could say, is another form of TMT.
---
+++ js/layers.js
@@ -0,0 +1,52 @@
+addLayer("1layer small", {// Add a * small* to generate a slightly different layer
+    name: "sideLayer1",
+    position: -1,
+    row: 0,
+    symbol() {return (options.ch || modInfo.languageMod==false) ? '↓ 层级 1 ↓' : '↓ layer 1 ↓'},
+    symbolEN() {return (options.ch || modInfo.languageMod==false) ? '↓ 层级 1 ↓' : '↓ layer 1 ↓'},
+    nodeStyle: {"font-size": "15px", "text-center": "center", "height": "30px"},
+    startData() { return {
+        unlocked: true,
+        small: true,
+        points: new Decimal(0),// This actually does nothing, but you have to write this. (Unless you want add something in this layer. #Todo, might change that later.)
+    }},
+    color: "#fefefe",
+    type: "none",
+    tooltip(){return false},
+    layerShown(){return layerDisplayTotal(['p'])},// If any layer in the array is unlocked, it will returns true. Otherwise it will return false.
+	tabFormat: [
+        ["display-text", function() { return getPointsDisplay() }]
+    ],
+})
+
+addLayer("p", {
+    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id
+    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+    symbolEN: "P", // The second name of this appears on the layer's node ( If you open otherLanguageMod )
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#4BDC13",
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    resource: "prestige points", // Name of prestige currency
+    resourceEN: "prestige points", // The second name of prestige currency ( If you open otherLanguageMod )
+    baseResource: "points", // Name of resource prestige is based on
+    baseResourceEN: "points", // The second name of resource prestige is based on ( If you open otherLanguageMod )
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 0, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true},
+})
